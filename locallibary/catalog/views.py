b7348da78fda78da2e6678e01b01dfe8bee74ed3
FaneTheEternal <artemyev.vova@gmail.com>
2019-12-07 23:01:03 +0300

    base generic

--- locallibary/catalog/views.py
+++ locallibary/catalog/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
+
 
 # Create your views here.
+def index(request):
+    """
+    Функция отображения для домашней страници
+    """
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    num_instances_available = BookInstance.objects.filter(status_exact='a').count()
+    num_author = Author.objects.count()  # all() run default
+
+    return render(
+        request,
+        'index.html',
+        context={
+            'num_books': num_books,
+            'num_instances': num_instances,
+            'num_instances_available': num_instances_available,
+            'num_author': num_author,
+            },
+    )
