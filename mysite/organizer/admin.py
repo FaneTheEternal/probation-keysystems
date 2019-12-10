ccf029a694d8d95c1dbad76ebdd94a0c0f5be517
FaneTheEternal <artemyev.vova@gmail.com>
2019-12-10 22:53:09 +0300

    add superuser

--- mysite/organizer/admin.py
+++ mysite/organizer/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Event, EventOptions, Participant
+
 
 # Register your models here.
+admin.site.register(Event)
+admin.site.register(EventOptions)
+admin.site.register(Participant)
