d6cdcb0aef074f330c37f52d1c593b9ce9a9c97b
FaneTheEternal <artemyev.vova@gmail.com>
2019-12-10 22:28:03 +0300

    add Event model

--- mysite/organizer/models.py
+++ mysite/organizer/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
+
 
 # Create your models here.
+class Event(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        help_text='Event ID',
+    )
+
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+    )
+
+    title = models.CharField(max_length=200)
