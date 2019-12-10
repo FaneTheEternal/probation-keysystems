1b9f1ab7cc6410aba4f592df1f2dcb3e010fc281
FaneTheEternal <artemyev.vova@gmail.com>
2019-12-10 22:41:37 +0300

    add EventOptions model

--- mysite/organizer/models.py
+++ mysite/organizer/models.py
@@ -17,3 +17,23 @@ class Event(models.Model):
     )
 
     title = models.CharField(max_length=200)
+
+
+class EventOptions(models.Model):
+    event = models.OneToOneField(
+        "Event",
+        on_delete=models.CASCADE
+    )
+
+    allow_wife = models.BooleanField()
+    allow_family = models.BooleanField()
+    for_kids = models.BooleanField()
+    size = models.IntegerField()
+    date = models.DateField(null=True, blank=True)
+    prepay_date = models.DateField(null=True, blank=True)
+    need_transport = models.BooleanField()
+    transport = models.CharField(max_length=200)
+    transport_size = models.IntegerField()
+    main_price = models.IntegerField()
+    other_prices = models.CharField(max_length=200)
+    deposit = models.IntegerField()
