85b455874f5c22c234d842daa61cb93e62270cff
FaneTheEternal <artemyev.vova@gmail.com>
2019-12-11 13:07:38 +0300

    add event list & event detail on urls

--- mysite/organizer/urls.py
+++ mysite/organizer/urls.py
@@ -8,4 +8,14 @@ urlpatterns = [
         views.index,
         name='index'
     ),
+    url(
+        r'^events/$',
+        views.EventListView.as_view(),
+        name='events',
+    ),
+    url(
+        r'^event/(?P<pk>\d+)$',
+        views.EventDetailView.as_view(),
+        name='event-detail',
+    ),
 ]
