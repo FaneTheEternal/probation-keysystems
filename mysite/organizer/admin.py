69b3e61000aedb6420fc841d9fa529ef9e1047eb
FaneTheEternal <artemyev.vova@gmail.com>
2019-12-11 16:52:44 +0300

    re register participant on admin

--- mysite/organizer/admin.py
+++ mysite/organizer/admin.py
@@ -4,4 +4,18 @@ from .models import Event, Participant
 
 # Register your models here.
 admin.site.register(Event)
-admin.site.register(Participant)
+# admin.site.register(Participant)
+
+
+@admin.register(Participant)
+class ParticipantAdmin(admin.ModelAdmin):
+    list_display = ('user', 'event', 'confirm', 'some_custom_data')
+    list_filter = ('user', 'event')
+    fieldsets = (
+        (None, {
+            'fields': ('event', 'confirm', 'some_custom_data')
+        }),
+        ('Availability', {
+            'fields': ('user',)
+        }),
+    )
