dbe03a8275f23053e3a9a3b4c191b61c2fa2c12a
FaneTheEternal <artemyev.vova@gmail.com>
2020-01-21 15:29:54 +0300

    перемещение модели профиля в отдельное приложение

--- mysite/uszver/models.py
+++ mysite/uszver/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.urls import reverse
+
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_moderator = models.BooleanField(default=False)
+
+    def __str__(self):
+        return '{0} {1}'.format(self.user.first_name, self.user.last_name)
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        is_moder = instance.is_superuser
+        if created:
+            Profile.objects.create(user=instance, is_moderator=is_moder)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+    def get_absolute_url(self):
+        return reverse("user-detail", args=[str(self.id)])
