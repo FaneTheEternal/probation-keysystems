65f7dc1a205683e57d49ecfba3cb4a5941be09c9
FaneTheEternal <artemyev.vova@gmail.com>
2020-01-23 23:31:29 +0300

    миграция и начало сореализации вормы событий

--- mysite/organizer/forms.py
+++ mysite/organizer/forms.py
@@ -1,1 +1,19 @@
 from django import forms
+
+
+class EventForm(forms.Form):
+    title = forms.CharField()
+
+    allow_wife = forms.BooleanField()
+    allow_family = forms.BooleanField()
+    for_kids = forms.BooleanField()
+    number_of_participants = forms.IntegerField()
+    event_date = forms.DateField()
+    prepay_date = forms.DateField()
+    personal_transportation = forms.BooleanField()
+    company_transport = forms.TextField()
+    company_transport_size = forms.IntegerField()
+    main_price = forms.IntegerField()
+    other_prices = forms.TextField()
+    deposit = forms.IntegerField()
+    some_properties = forms.TextField()
