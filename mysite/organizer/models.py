2f03fd620386af6ab07d2dbfdc3120aa52099297
FaneTheEternal <artemyev.vova@gmail.com>
2020-01-23 23:59:42 +0300

    конфликт версий устанен, замена на современное решение

--- mysite/organizer/models.py
+++ mysite/organizer/models.py
@@ -55,12 +55,9 @@ class Participant(models.Model):
         )
 
     class Meta:
-        """
         constraints = [
             models.UniqueConstraint(
                 fields=['user', 'event'],
                 name='unique_partic',
             ),
         ]
-        """
-        unique_together = (('user', 'event'),)
